32c36e5b803a8074442c3b90a0e4a73263c15c02
AtomMushroom <35073893+AtomMushroom@users.noreply.github.com>
2022-03-17 18:45:59 +0300

    Add files via upload

---
+++ Lesson_8Lab1/Lesson_8Lab1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Lesson_8Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstMenu = new System.Windows.Forms.ListBox();
+            this.lstZakaz = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRemoveAll = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.txtNew = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstMenu
+            // 
+            this.lstMenu.FormattingEnabled = true;
+            this.lstMenu.ItemHeight = 16;
+            this.lstMenu.Items.AddRange(new object[] {
+            "Доширак (60)",
+            "Тортик (120)",
+            "Супчик (80)",
+            "Пицца (120)",
+            "Шаурма (160)",
+            "Самса (70)"});
+            this.lstMenu.Location = new System.Drawing.Point(16, 84);
+            this.lstMenu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstMenu.Name = "lstMenu";
+            this.lstMenu.Size = new System.Drawing.Size(221, 148);
+            this.lstMenu.TabIndex = 0;
+            // 
+            // lstZakaz
+            // 
+            this.lstZakaz.FormattingEnabled = true;
+            this.lstZakaz.ItemHeight = 16;
+            this.lstZakaz.Location = new System.Drawing.Point(415, 84);
+            this.lstZakaz.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstZakaz.Name = "lstZakaz";
+            this.lstZakaz.Size = new System.Drawing.Size(221, 196);
+            this.lstZakaz.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(247, 84);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(160, 28);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(244, 251);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(163, 28);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "Удалить";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(513, 289);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(123, 34);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "Заказ";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 59);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "МЕНЮ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(479, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 59);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ЗАКАЗ";
+            // 
+            // btnRemoveAll
+            // 
+            this.btnRemoveAll.Location = new System.Drawing.Point(248, 287);
+            this.btnRemoveAll.Name = "btnRemoveAll";
+            this.btnRemoveAll.Size = new System.Drawing.Size(159, 23);
+            this.btnRemoveAll.TabIndex = 7;
+            this.btnRemoveAll.Text = "Убрать все";
+            this.btnRemoveAll.UseVisualStyleBackColor = true;
+            this.btnRemoveAll.Click += new System.EventHandler(this.btnRemoveAll_Click);
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(16, 299);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(143, 23);
+            this.btnAddNew.TabIndex = 8;
+            this.btnAddNew.Text = "Добавить блюдо";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
+            // 
+            // txtNew
+            // 
+            this.txtNew.Location = new System.Drawing.Point(16, 251);
+            this.txtNew.Name = "txtNew";
+            this.txtNew.Size = new System.Drawing.Size(221, 22);
+            this.txtNew.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 335);
+            this.Controls.Add(this.txtNew);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.btnRemoveAll);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstZakaz);
+            this.Controls.Add(this.lstMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Две самсы (печеньки)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstMenu;
+        private System.Windows.Forms.ListBox lstZakaz;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRemoveAll;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.TextBox txtNew;
+    }
+}
+
